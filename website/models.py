50b9c02d897a842da8788dd87def38e9ad124b60
Mohammed <mosminecraft4@gmail.com>
2021-06-09 00:26:01 +0100

    Fix

--- website/models.py
+++ website/models.py
@@ -2,22 +2,27 @@ from django.db import models
 
 # Create your models here.
 class First_prize(models.Model):
-    bnb = models.TextField(max_length=200, null=True,blank=True)
+    name = models.CharField(max_length=200, null=True,blank=True)
+    bnb = models.CharField(max_length=200, null=True,blank=True)
 
     def __str__(self):
-        return self.bnb
+        return self.name
     
 
 class Second_prize(models.Model):
-    bnb = models.TextField(max_length=200, null=True,blank=True)
+    name = models.CharField(max_length=200, null=True,blank=True)
+    bnb = models.CharField(max_length=200, null=True,blank=True)
 
     def __str__(self):
-        return self.bnb
+        return self.name
     
 
 class Third_prize(models.Model):
-    bnb = models.TextField(max_length=200, null=True,blank=True)
+    name = models.CharField(max_length=200, null=True,blank=True)
+    bnb = models.CharField(max_length=200, null=True,blank=True)
+
 
     def __str__(self):
-        return self.bnb
+        return self.name
     
+
